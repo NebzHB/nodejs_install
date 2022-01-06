3444b2fa29f89b3a7301e952285e75a23963b5e3
Nebz <28622481+NebzHB@users.noreply.github.com>
2022-01-06 10:32:41 +0100

    Update install_nodejs.sh

--- install_nodejs.sh
+++ install_nodejs.sh
@@ -216,16 +216,16 @@ fi
 
 if [[ "$2" == "nvm" ]]; then
   step 46 "Installation NVM"
-  #rm -f ${HOME}/.npmrc
-  wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-  rm -f install.sh
+  silent rm -f ${HOME}/.npmrc
+  wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | try bash
+  silent rm -f install.sh
   source ~/.bashrc
-  nvm alias default system
-  nvm use system
+  try nvm alias default system
+  try nvm use system
   if [ ! -z $3 ]; then
-  	nvm install $3
-	nvm alias default system
-	nvm use system
+  	try nvm install $3
+	try nvm alias default system
+	try nvm use system
   fi
 fi
 

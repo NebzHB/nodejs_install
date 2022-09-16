bf214badba4bbbad228fb7f7920a680c21d82b89
Nebz <28622481+NebzHB@users.noreply.github.com>
2022-09-16 14:21:17 +0200

    Update install_nodejs.sh

--- install_nodejs.sh
+++ install_nodejs.sh
@@ -3,7 +3,11 @@
 installVer=$1 	#NodeJS major version to be installed
 minVer=$1	#min NodeJS major version to be accepted
 
-step 10 "Prérequis"
+if [ "$LANG_DEP" = "fr" ]; then
+	step 10 "Prérequis"
+else
+	step 10 "Prerequisites"
+fi
 # vérifier si toujours nécessaire, cette source traine encore sur certaines smart et si une source est invalide -> nodejs ne s'installera pas
 
 #prioritize nodesource nodejs : just in case
@@ -13,51 +17,81 @@ Pin: origin deb.nodesource.com
 Pin-Priority: 600
 EOL
 
-step 15 "Installation des packages nécessaires"
+if [ "$LANG_DEP" = "fr" ]; then
+	step 15 "Installation des packages nécessaires"
+else
+	step 15 "Mandatory packages installation"
+fi
 # apt-get update should have been done in the calling file
 try sudo DEBIAN_FRONTEND=noninteractive apt-get install -y lsb-release build-essential apt-utils git
 
-step 20 "Vérification du système"
+if [ "$LANG_DEP" = "fr" ]; then
+	step 20 "Vérification du système"
+else
+	step 20 "System Check"
+fi
 arch=`arch`;
 
 #jessie as libstdc++ > 4.9 needed for nodejs 12+
 lsb_release -c | grep jessie
-if [ $? -eq 0 ]
-then
+if [ $? -eq 0 ]; then
   today=$(date +%Y%m%d)
-  if [[ "$today" > "20200630" ]]; 
-  then 
-    echo "$HR"
-    echo "== KO == Erreur d'Installation"
-    echo "$HR"
-    echo "== ATTENTION Debian 8 Jessie n'est officiellement plus supportée depuis le 30 juin 2020, merci de mettre à jour votre distribution !!!"
+  if [[ "$today" > "20200630" ]]; then
+    if [ "$LANG_DEP" = "fr" ]; then
+      echo "$HR"
+      echo "== KO == Erreur d'Installation"
+      echo "$HR"
+      echo "== ATTENTION Debian 8 Jessie n'est officiellement plus supportée depuis le 30 juin 2020, merci de mettre à jour votre distribution !!!"
+    else
+      echo "$HR"
+      echo "== KO == Installation Error"
+      echo "$HR"
+      echo "== WARNING Debian 8 Jessie is not supported anymore since the 30rd of june 2020, thank you to update your distribution !!!"
+    fi
     exit 1
   fi
 fi
 
 #x86 32 bits not supported by nodesource anymore
 bits=$(getconf LONG_BIT)
-if { [ "$arch" = "i386" ] || [ "$arch" = "i686" ]; } && [ "$bits" -eq "32" ]
-then 
-  echo "$HR"
-  echo "== KO == Erreur d'Installation"
-  echo "$HR"
-  echo "== ATTENTION Votre système est x86 en 32bits et NodeJS 12 n'y est pas supporté, merci de passer en 64bits !!!"
+if { [ "$arch" = "i386" ] || [ "$arch" = "i686" ]; } && [ "$bits" -eq "32" ]; then
+  if [ "$LANG_DEP" = "fr" ]; then
+    echo "$HR"
+    echo "== KO == Erreur d'Installation"
+    echo "$HR"
+    echo "== ATTENTION Votre système est x86 en 32bits et NodeJS 12 n'y est pas supporté, merci de passer en 64bits !!!"
+  else
+    echo "$HR"
+    echo "== KO == Installation Error"
+    echo "$HR"
+    echo "== WARNING Your system is x86 in 32bits and NodeJS 12 doesn not support it anymore, thank you to reinstall in 64bits !!!"
+  fi
   exit 1 
 fi
 
-step 25 "Vérification de la version de NodeJS installée"
+if [ "$LANG_DEP" = "fr" ]; then
+	step 25 "Vérification de la version de NodeJS installée"
+else
+	step 25 "Installed NodeJS version check"
+fi
 silent type node
 if [ $? -eq 0 ]; then actual=`node -v`; else actual='Aucune'; fi
 testVer=$(php -r "echo version_compare('${actual}','v${minVer}','>=');")
-echo -n "[Check Version NodeJS actuelle : ${actual} : "
-if [[ $testVer == "1" ]]
-then
+if [ "$LANG_DEP" = "fr" ]; then
+	echo -n "[Check Version NodeJS actuelle : ${actual} : "
+else
+	echo -n "[Check Current NodeJS Version : ${actual} : "
+fi
+if [[ $testVer == "1" ]]; then
   echo "[  OK  ]";
   new=$actual
 else
   echo "[  KO  ]";
-  step 30 "Installation de NodeJS $installVer"
+  if [ "$LANG_DEP" = "fr" ]; then
+  	step 30 "Installation de NodeJS $installVer"
+  else
+  	step 30 "Installing NodeJS $installVer"
+  fi
   
   #if npm exists
   silent type npm
@@ -71,30 +105,24 @@ else
   silent sudo DEBIAN_FRONTEND=noninteractive apt-get -y --purge autoremove nodejs
   
   
-  if [[ $arch == "armv6l" ]]
-  then
+  if [[ $arch == "armv6l" ]]; then
     #version to install for armv6 (to check on https://unofficial-builds.nodejs.org)
-    if [[ $installVer == "12" ]]
-    then
+    if [[ $installVer == "12" ]]; then
       armVer="12.22.12"
-    fi
-    if [[ $installVer == "13" ]]
-    then
+    elif [[ $installVer == "13" ]]; then
       armVer="13.14.0"
-    fi
-    if [[ $installVer == "14" ]]
-    then
+    elif [[ $installVer == "14" ]]; then
       armVer="14.19.2"
-    fi
-    if [[ $installVer == "15" ]]
-    then
+    elif [[ $installVer == "15" ]]; then
       armVer="15.14.0"
-    fi
-    if [[ $installVer == "16" ]]
-    then
+    elif [[ $installVer == "16" ]]; then
       armVer="16.15.0"
     fi
-    echo "Jeedom Mini ou Raspberry 1, 2 ou zéro détecté, non supporté mais on essaye l'utilisation du paquet non-officiel v${armVer} pour armv6l"
+    if [ "$LANG_DEP" = "fr" ]; then
+    	echo "Jeedom Mini ou Raspberry 1, 2 ou zéro détecté, non supporté mais on essaye l'utilisation du paquet non-officiel v${armVer} pour armv6l"
+    else
+    	echo "Jeedom Mini or Raspberry 1, 2 or zero detected, unsupported but we try to install unofficial packet v${armVer} for armv6l"
+    fi
     try wget https://unofficial-builds.nodejs.org/download/release/v${armVer}/node-v${armVer}-linux-armv6l.tar.gz
     try tar -xvf node-v${armVer}-linux-armv6l.tar.gz
     cd node-v${armVer}-linux-armv6l
@@ -106,7 +134,11 @@ else
     #upgrade to recent npm
     try sudo npm install -g npm
   else
-    echo "Utilisation du dépot officiel"
+    if [ "$LANG_DEP" = "fr" ]; then
+    	echo "Utilisation du dépot officiel"
+    else
+    	echo "Using official repository"
+    fi
     curl -fsSL https://deb.nodesource.com/setup_${installVer}.x | try sudo -E bash -
     try sudo DEBIAN_FRONTEND=noninteractive apt-get install -y nodejs 
   fi
@@ -114,11 +146,18 @@ else
   silent npm config set prefix ${npmPrefix}
 
   new=`node -v`;
-  echo -n "[Check Version NodeJS après install : ${new} : "
+  if [ "$LANG_DEP" = "fr" ]; then
+  	echo -n "[Check Version NodeJS après install : ${new} : "
+  else
+  	echo -n "[Check NodeJS Version after install : ${new} : "
+  fi
   testVerAfter=$(php -r "echo version_compare('${new}','v${minVer}','>=');")
-  if [[ $testVerAfter != "1" ]]
-  then
-    echo "[  KO  ] -> relancez les dépendances"
+  if [[ $testVerAfter != "1" ]]; then
+    if [ "$LANG_DEP" = "fr" ]; then
+    	echo "[  KO  ] -> relancez les dépendances"
+    else
+    	echo "[  KO  ] -> restart the dependancies"
+    fi
   else
     echo "[  OK  ]"
   fi
@@ -126,7 +165,11 @@ fi
 
 silent type npm
 if [ $? -ne 0 ]; then
-  step 40 "Installation de npm car non présent"
+  if [ "$LANG_DEP" = "fr" ]; then
+  	step 40 "Installation de npm car non présent"
+  else
+  	step 40 "Installing npm because not present"
+  fi
   try sudo DEBIAN_FRONTEND=noninteractive apt-get install -y npm  
   try sudo npm install -g npm
 fi
@@ -136,7 +179,11 @@ echo -n "[Check Version NPM : ${npmver} : "
 echo $npmver | grep "8.11.0" &>/dev/null
 if [ $? -eq 0 ]; then
 	echo "[  KO  ]"
-	step 42 "Mise à jour de NPM vers la 8.12.2"
+	if [ "$LANG_DEP" = "fr" ]; then
+		step 42 "Mise à jour de NPM vers la 8.12.2"
+	else
+		step 42 "Updating NPM to version 8.12.2"
+	fi
 	try sudo npm install -g npm@8.12.2
 else
 	echo "[  OK  ]"
@@ -147,23 +194,43 @@ if [ $? -eq 0 ]; then
   npmPrefix=`npm --silent prefix -g`
   npmPrefixSudo=`sudo npm --silent prefix -g`
   npmPrefixwwwData=`sudo -u www-data npm --silent  prefix -g`
-  echo -n "[Check Prefix : $npmPrefix and sudo prefix : $npmPrefixSudo and www-data prefix : $npmPrefixwwwData : "
+  if [ "$LANG_DEP" = "fr" ]; then
+  	echo -n "[Check Prefixe : $npmPrefix et sudo prefixe : $npmPrefixSudo et www-data prefixe : $npmPrefixwwwData : "
+  else
+  	echo -n "[Check Prefix : $npmPrefix and sudo prefix : $npmPrefixSudo and www-data prefix : $npmPrefixwwwData : "
+  fi
   if [[ "$npmPrefixSudo" != "/usr" ]] && [[ "$npmPrefixSudo" != "/usr/local" ]]; then 
     echo "[  KO  ]"
     if [[ "$npmPrefixwwwData" == "/usr" ]] || [[ "$npmPrefixwwwData" == "/usr/local" ]]; then
-      step 45 "Reset prefix ($npmPrefixwwwData) pour npm `sudo whoami`"
+      if [ "$LANG_DEP" = "fr" ]; then
+      	step 45 "Réinitialisation prefixe ($npmPrefixwwwData) pour npm `sudo whoami`"
+      else
+        step 45 "Prefix reset ($npmPrefixwwwData) for npm `sudo whoami`"
+      fi
       sudo npm config set prefix $npmPrefixwwwData
     else
       if [[ "$npmPrefix" == "/usr" ]] || [[ "$npmPrefix" == "/usr/local" ]]; then
-        step 45 "Reset prefix ($npmPrefix) pour npm `sudo whoami`"
+        if [ "$LANG_DEP" = "fr" ]; then
+        	step 45 "Réinitialisation prefixe ($npmPrefix) pour npm `sudo whoami`"
+	else
+		step 45 "Prefix reset ($npmPrefix) for npm `sudo whoami`"
+	fi
         sudo npm config set prefix $npmPrefix
       else
         [ -f /usr/bin/raspi-config ] && { rpi="1"; } || { rpi="0"; }
         if [[ "$rpi" == "1" ]]; then
-	  step 45 "Reset prefix (/usr) pour npm `sudo whoami`"
+	  if [ "$LANG_DEP" = "fr" ]; then
+	  	step 45 "Réinitialisation prefixe (/usr) pour npm `sudo whoami`"
+	  else
+	  	step 45 "Prefix reset (/usr) for npm `sudo whoami`"
+	  fi
           sudo npm config set prefix /usr
 	else
-          step 45 "Reset prefix (/usr/local) pour npm `sudo whoami`"
+	  if [ "$LANG_DEP" = "fr" ]; then
+	  	step 45 "Réinitialisation prefixe (/usr/local) pour npm `sudo whoami`"
+	  else
+          	step 45 "Prefix reset (/usr/local) for npm `sudo whoami`"
+	  fi
           sudo npm config set prefix /usr/local
 	fi
       fi
@@ -174,21 +241,37 @@ if [ $? -eq 0 ]; then
         echo "[  OK  ]"
       else
         echo "[  KO  ]"
-        step 45 "Reset prefix ($npmPrefixwwwData) pour npm `sudo whoami`"
+	if [ "$LANG_DEP" = "fr" ]; then
+        	step 45 "Réinitialisation prefixe ($npmPrefixwwwData) pour npm `sudo whoami`"
+	else
+		step 45 "Prefix reset ($npmPrefixwwwData) for npm `sudo whoami`"
+	fi
         sudo npm config set prefix $npmPrefixwwwData
       fi
     else
       echo "[  KO  ]"
       if [[ "$npmPrefix" == "/usr" ]] || [[ "$npmPrefix" == "/usr/local" ]]; then
-        step 45 "Reset prefix ($npmPrefix) pour npm `sudo whoami`"
+        if [ "$LANG_DEP" = "fr" ]; then
+        	step 45 "Réinitialisation prefixe ($npmPrefix) pour npm `sudo whoami`"
+	else
+		step 45 "Prefix reset ($npmPrefix) for npm `sudo whoami`"
+	fi
         sudo npm config set prefix $npmPrefix
       else
         [ -f /usr/bin/raspi-config ] && { rpi="1"; } || { rpi="0"; }
         if [[ "$rpi" == "1" ]]; then
-	  step 45 "Reset prefix (/usr) pour npm `sudo whoami`"
+	  if [ "$LANG_DEP" = "fr" ]; then
+	  	step 45 "Réinitialisation prefixe (/usr) pour npm `sudo whoami`"
+	  else
+	  	step 45 "Prefix reset (/usr) for npm `sudo whoami`"
+	  fi
           sudo npm config set prefix /usr
 	else
-          step 45 "Reset prefix (/usr/local) pour npm `sudo whoami`"
+	  if [ "$LANG_DEP" = "fr" ]; then
+          	step 45 "Réinitialisation prefixe (/usr/local) pour npm `sudo whoami`"
+	  else
+		step 45 "Prefix reset (/usr/local) for npm `sudo whoami`"
+	  fi
           sudo npm config set prefix /usr/local
 	fi
       fi
@@ -196,7 +279,11 @@ if [ $? -eq 0 ]; then
   fi
 fi
 
-step 50 "Nettoyage"
+if [ "$LANG_DEP" = "fr" ]; then
+	step 50 "Nettoyage"
+else
+	step 50 "Cleaning"
+fi
 # on nettoie la priorité nodesource
 silent sudo rm -f /etc/apt/preferences.d/nodesource
 
